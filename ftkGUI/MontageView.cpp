d6bf3afb8bbd2c1eee070eb301cb6ab13b8b05e7
Kedar Grama <gbkedar@uh.edu>
2013-01-13 17:12:20 -0600

    Moving between sys for montage view

---
+++ ftkGUI/MontageView.cpp
@@ -0,0 +1,74 @@
+MontageView::MontageView( QWidget * parent )
+{
+  createMenus();
+}
+
+void MontageView::createMenus()
+{
+  fileMenu = menuBar()->addMenu(tr("&File"));
+  fileMenu->setObjectName("fileMenu");
+
+  loadProjectAction = new QAction(tr("Load Project"), this);
+  loadProjectAction->setObjectName("loadProjectAction");
+  loadProjectAction->setStatusTip(tr("Load ftk project"));
+  loadProjectAction->setShortcut(tr("Ctrl+L"));
+  connect(loadProjectAction, SIGNAL(triggered()), this, SLOT(loadProject()));
+  fileMenu->addAction(loadProjectAction);
+
+  loadImageAction = new QAction(tr("Load Xml Image"), this);
+  loadImageAction->setObjectName("loadImageAction");
+  loadImageAction->setStatusTip(tr("Load an xml image"));
+  loadImageAction->setShortcut(tr("Ctrl+O"));
+  connect(loadImageAction, SIGNAL(triggered()), this, SLOT(askLoadImage()));
+  fileMenu->addAction(loadImageAction);
+
+  fileMenu->addSeparator();
+
+  exitAction = new QAction(tr("Exit"), this);
+  exitAction->setObjectName("exitAction");
+  exitAction->setShortcut(tr("Ctrl+Q"));
+  exitAction->setStatusTip(tr("Close the widget"));
+  connect(exitAction, SIGNAL(triggered()), this, SLOT(close()));
+  fileMenu->addAction(exitAction);
+
+  //VIEW MENU
+  viewMenu = menuBar()->addMenu(tr("&View"));
+  viewMenu->setObjectName("viewMenu");
+  displayChannelMenu = viewMenu->addMenu(tr("Display Channel"));
+  displayChannelMenu->setObjectName("displayChannelMenu");
+  connect(displayChannelMenu, SIGNAL(aboutToShow()), this, SLOT(DisplayChannelsMenu()));
+
+}
+
+void MontageView::DisplayChannelsMenu()
+{
+  if( !SubsampledImage )
+    return;
+
+  std::vector<std::string> channel_names = SubsampledImage->GetChannelNames();
+  std::vector<bool> channel_status = montDisp->GetChannelFlags();
+
+  //remove all existing display channel actions
+  for(unsigned i=0; i<displayChannelAction.size(); ++i)
+    delete displayChannelAction.at(i);
+  displayChannelMenu->clear();
+  displayChannelAction.clear();
+
+  if(chSignalMapper)
+    delete chSignalMapper;
+
+  chSignalMapper = new QSignalMapper(this);
+  for(unsigned i=0; i<channel_names.size(); ++i)
+  {
+    QAction * action = new QAction( tr(channel_names.at(i).c_str()), this );
+    action->setObjectName(tr(channel_names.at(i).c_str()));
+    action->setCheckable(true);
+    action->setChecked( channel_status.at(i) );
+    action->setStatusTip( tr("Turn on/off this channel") );
+    action->setShortcut( QString::number(i) );
+    connect(action, SIGNAL(triggered()), chSignalMapper, SLOT(map()));
+    chSignalMapper->setMapping( action, i );
+    displayChannelMenu->addAction(action);
+  }
+  connect(chSignalMapper, SIGNAL(mapped(int)), this, SLOT(toggleChannel(int)));
+}
---
+++ ftkGUI/MontageView.h
@@ -0,0 +1,57 @@
+#ifndef MONTAGE_VIEW_H
+#define MONTAGE_VIEW_H
+
+#include <iostream>
+
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenuBar>
+#include <QtGui/QMenu>
+#include <QtGui/QAction>
+#include <QtGui/QWidget>
+#include <QApplication>
+#include <QFileDialog>
+
+class MontageView : public QMainWindow
+{
+  Q_OBJECT;
+public:
+  MontageView(QWidget * parent = 0);
+  ~MontageView();
+  void Initialize();
+
+private:
+
+protected:
+  virtual void closeEvent(QCloseEvent *event);
+protected slots:
+  void loadProject(void);
+  void askLoadImage(void);
+  void DisplayChannelsMenu(void);
+  void toggleChannel(int chNum);
+  void loadImage(QString fileName);
+
+
+private:
+  QVTKWidget MainQTWidget;
+  ftk::Image::Pointer Image;
+  ftk::Image::Pointer SubsampledImage;
+  ftk::Image::Pointer LabelImage;
+  vtkSmartPointer<vtkTable> Table;
+
+signals:
+protected:
+  void createMenus();
+  QMenu *fileMenu;
+  QAction *loadProjectAction;
+  QAction *loadImageAction;
+  QAction *exitAction;
+
+  QMenu *viewMenu;
+  QMenu *displayChannelMenu;
+  QSignalMapper *chSignalMapper;
+  QVector<QAction *> displayChannelAction;
+
+}
+
+
+#endif //MONTAGE_VIEW_H
